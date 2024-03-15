133d482007790a678711b0f728f3a7815861ab3a
Khizer Rehan <khizerrehan92@gmail.com>
2024-03-16 03:11:12 +0500

    Added documentation tool

--- src/lib/data/tools.data.ts
+++ src/lib/data/tools.data.ts
@@ -1,6 +1,7 @@
 export enum Catgeory {
   Frontend = "frontend",
   Backend = "backend",
+  Documentation = "documentaion",
 }
 
 export const Tools = [
@@ -24,5 +25,13 @@ export const Tools = [
     description:
       "Nx is a build system with built-in tooling and advanced CI capabilities . It helps you maintain and scale monorepos, both locally and on CI.",
     category: Catgeory.Frontend,
+  },
+
+  {
+    name: "nextra",
+    href: "https://nextra.site/",
+    description:
+      "Simple, powerful and flexible site generation framework with everything you love from Next.js",
+    category: Catgeory.Documentation,
   },
 ];
