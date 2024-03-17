5ad7bcadc705dcf2d7cbaa4b48264e05938a7d94
Khizer Rehan <khizerrehan92@gmail.com>
2024-03-18 03:41:38 +0500

    Add tailwind cheatsheet link

--- src/lib/data/cheatsheet/cheatsheet.data.ts
+++ src/lib/data/cheatsheet/cheatsheet.data.ts
@@ -1,4 +1,5 @@
 const githubTag = "github";
+const tailwindTag = "tailwind";
 export const CheatSheetData = [
   {
     name: "GitHub CLI manual",
@@ -6,5 +7,12 @@ export const CheatSheetData = [
     description:
       "GitHub CLI, or gh, is a command-line interface to GitHub for use in your terminal or your scripts.",
     category: githubTag,
+  },
+  {
+    name: "Tailwind CSS Cheat Sheet",
+    href: "https://tailwindcomponents.com/cheatsheet/",
+    description:
+      "Find quickly all the class names and CSS properties with this interactive cheat sheet",
+    category: tailwindTag,
   },
 ];
